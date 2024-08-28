00f3d1f26bf9025fae3dccbd42eeb0fb9694ca79
larads <dasilval.mariana@gmail.com>
2024-08-28 11:25:26 -0400

    ✨ feat: Testes e controller de autenticação

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,55 @@
+import { hash } from 'bcryptjs'
+import { expect, describe, it } from 'vitest'
+import { AuthenticateUseCase } from '../use-cases/authenticate'
+import { InMemoryUsersRepository } from '../repositories/in-memory/in-memory-users-repository'
+import { InvalidCredentialsError } from '../use-cases/errors/invalid-credentials-error'
+
+describe('Authenticate Use Case', () => {
+    it('should be able to authenticate', async () => {
+        const usersRepository = new InMemoryUsersRepository()
+        const sut = new AuthenticateUseCase(usersRepository)
+
+        await usersRepository.create({
+            name: 'Mariana Lara',
+            email: 'mariana@gmail.com',
+            password_hash: await hash('123456', 6),
+        })
+
+        const { user } = await sut.execute({
+            email: 'mariana@gmail.com',
+            password: '123456',
+        })
+
+        expect(user.id).toEqual(expect.any(String))
+    })
+
+    it('should not be able to authenticate with wrong email', async () => {
+        const usersRepository = new InMemoryUsersRepository()
+        const sut = new AuthenticateUseCase(usersRepository)
+
+        expect(() =>
+            sut.execute({
+                email: 'mariana@gmail.com',
+                password: '123456',
+            }),
+        ).rejects.toBeInstanceOf(InvalidCredentialsError)
+    })
+
+    it('should not be able to authenticate with wrong email', async () => {
+        const usersRepository = new InMemoryUsersRepository()
+        const sut = new AuthenticateUseCase(usersRepository)
+
+        await usersRepository.create({
+            name: 'Mariana Lara',
+            email: 'mariana@gmail.com',
+            password_hash: await hash('123456', 6),
+        })
+
+        expect(() =>
+            sut.execute({
+                email: 'mariana@gmail.com',
+                password: '123123',
+            }),
+        ).rejects.toBeInstanceOf(InvalidCredentialsError)
+    })
+})

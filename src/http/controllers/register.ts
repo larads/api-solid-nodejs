7828a2354c6aa0f332ce30dd1e98ef551da027e5
larads <dasilval.mariana@gmail.com>
2024-08-28 10:32:07 -0400

    ✨ feat: Handler de erros global

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod'
-import { prisma } from '../../lib/prisma'
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { RegisterUseCase } from '../../use-cases/register'
+import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '../../use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerBodySchema = z.object({
@@ -8,16 +10,24 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
         email: z.string().email(),
         password: z.string().min(6),
     })
-
     const { name, email, password } = registerBodySchema.parse(request.body)
 
-    await prisma.user.create({
-        data: {
+    try {
+        const usersRepository = new PrismaUsersRepository()
+        const registerUseCase = new RegisterUseCase(usersRepository)
+
+        await registerUseCase.execute({
             name,
             email,
-            password_hash: password,
-        },
-    })
+            password,
+        })
+    } catch (err) {
+        if (err instanceof UserAlreadyExistsError) {
+            return reply.status(409).send({ message: err.message })
+        }
+
+        throw err
+    }
 
     return reply.status(201).send()
 }

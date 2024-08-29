46bcbe425eb16ccd73efd33e9e7889addfbccd3f
larads <dasilval.mariana@gmail.com>
2024-08-29 11:37:41 -0400

    ✨ feat: Controller de perfil

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,10 +1,11 @@
 import { FastifyInstance } from 'fastify'
-import { register } from './controllers/register'
-import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
+import { register } from './controllers/register'
+import { verifyJwt } from '../middlewares/verify-jwt'
+import { authenticate } from './controllers/authenticate'
 
 export async function appRoutes(app: FastifyInstance) {
-    app.get('/me', profile)
+    app.get('/me', { onRequest: [verifyJwt] }, profile)
     app.post('/users', register)
     app.post('/sessions', authenticate)
 }

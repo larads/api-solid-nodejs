d32cafd11d1e786df012e7c9d40f9391ed0a724c
larads <dasilval.mariana@gmail.com>
2024-08-29 11:32:23 -0400

    ✨ feat: Implementando JWT no Fastify

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import { env } from './env'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
+import fastifyJwt from '@fastify/jwt'
 import { appRoutes } from './http/routes'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET,
+})
 
 app.register(appRoutes)
 
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
+    app.get('/me', profile)
     app.post('/users', register)
     app.post('/sessions', authenticate)
 }
